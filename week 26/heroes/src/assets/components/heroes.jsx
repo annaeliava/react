e16855e54cfa4855546913daf71046504e0faae3
annaeliava <annaeliavad@gmail.com>
2022-09-10 02:09:16 +0300

    +

--- week 26/heroes/src/assets/components/heroes.jsx
+++ week 26/heroes/src/assets/components/heroes.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 function Heroes(props) {
     return (
         <div class='hero'>
-            <h2 class="heroProfile" id="heroName">{props.name}</h2>
-            <div class="heroProfile" id="universe">Вселенная: {props.universe} </div>
-            <div class="heroProfile" id="alterEgo">Альтер Эго: {props.alterEgo} </div>
-            <div class="heroProfile" id="activity">Род деятельности: {props.activity} </div>
-            <div class="heroProfile" id="friends">Друзья: {props.friends} </div>
-            <div class="heroProfile" id="siperPower">Суперсилы: {props.superPowers}</div>
-            <img class="heroProfile" alt="hero" src='{props.pic}' />
-            <div class="heroProfile" id="description">Подробнее: {props.description}</div><br />
-            <div class="heroProfile">Оцените героя</div>
-            <select class="heroProfile" id="rating {props.name}">
+            <h2 className="heroProfile" id="heroName">{props.name}</h2>
+            <div className="heroProfile" id="universe">Вселенная: {props.universe} </div>
+            <div className="heroProfile" id="alterEgo">Альтер Эго: {props.alterEgo} </div>
+            <div className="heroProfile" id="activity">Род деятельности: {props.activity} </div>
+            <div className="heroProfile" id="friends">Друзья: {props.friends} </div>
+            <div className="heroProfile" id="siperPower">Суперсилы: {props.superPowers}</div>
+            <img className="heroProfile" alt="hero" src={props.pic} />
+            <div className="heroProfile" id="description">Подробнее: {props.description}</div><br />
+            <div className="heroProfile">Оцените героя</div>
+            <select className="heroProfile" id="rating {props.name}">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
