56a49abdb49c153a9c9fe8b917cbb2a4d2b3abb7
annaeliava <annaeliavad@gmail.com>
2022-10-18 02:26:56 +0300

    fixed

--- week 27/tarif/src/assets/components/tarif.jsx
+++ week 27/tarif/src/assets/components/tarif.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './TariffCard.module.css';
+import styles from './TariffCard.module.css';
 
 function Tarif(props) {
     return (
         <>
-            <div className={"card" + (props.isSelected ? "selected" : "")}>
-                <div className='title' style={{ backgroundColor: '{props.bg}' }}>Безлимитный {props.price} </div>
-                <div className='sub' style={{ backgroundColor: '{props.bg}' }}>
-                    <span id='rub'>руб</span>
-                    <span id='bold'>{props.price}</span>
-                    <span id='month'>/мес</span>
+            <div className={"{styles.card}" + (props.isSelected ? "{styles.selected}" : "")}>
+                <div className={styles.title} style={{ backgroundColor: '{props.bg}' }}>Безлимитный {props.price} </div>
+                <div className={styles.sub} style={{ backgroundColor: '{props.bg}' }}>
+                    <span id={styles.rub}>руб</span>
+                    <span id={styles.bold}>{props.price}</span>
+                    <span id={styles.month}>/мес</span>
                 </div>
-                <div className='price'>до {props.mb} Мбит/сек</div>
-                <div className='vol'>
+                <div className={styles.price}>до {props.mb} Мбит/сек</div>
+                <div className={styles.vol}>
                     <div>Объем включенного</div>
                     <div>трафика не ограничен</div>
                 </div>
