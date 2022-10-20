7ede66bf9d2f5b102117ee272b29860ead820cb2
annaeliava <annaeliavad@gmail.com>
2022-10-20 19:50:32 +0300

    corrected

--- week 27/tarif/src/App.js
+++ week 27/tarif/src/App.js
@@ -1,20 +1,20 @@
 import Tariff from "./assets/components/Tariff";
-import './assets/styles/app.css';
+import styles from "./assets/styles/App.module.css";
 
 const tariff_data = [
-  {price: '300', mbitQuantity: '10', backgroundRGB: '(0, 0, 255)'},
-  {price: '450', mbitQuantity: '50', backgroundRGB: '(0, 128, 0)'},
-  {price: '550', mbitQuantity: '100', backgroundRGB: '(255, 0, 0)', isSelected: true},
-  {price: '1000', mbitQuantity: '200', backgroundRGB: '(0, 0, 0)'}
+  {price: '300', mbitQuantity: '10', backgroundTitle: '4A89DC', backgroundPriceSection: '5D9CEC'},
+  {price: '450', mbitQuantity: '50', backgroundTitle: '37BC9B', backgroundPriceSection: '48CFAD'},
+  {price: '550', mbitQuantity: '100', backgroundTitle: 'DA4453', backgroundPriceSection: 'ED5565', isSelected: true},
+  {price: '1000', mbitQuantity: '200', backgroundTitle: '434A54', backgroundPriceSection: '656D78'},
 ];
 
 function App() {
   return (
     <>
-      <div className="container">
+      <div className={styles.container}>
         {
-          tariff_data.map((tariff_data) =>
-          <Tariff price = {tariff_data.price} mbitQuantity = {tariff_data.mbitQuantity} backgroundRGB = {tariff_data.backgroundRGB} isSelected = {tariff_data.isSelected} />
+          tariff_data.map(tariff_data =>
+          <Tariff price = {tariff_data.price} mbitQuantity = {tariff_data.mbitQuantity} backgroundTitle = {tariff_data.backgroundTitle} backgroundPriceSection ={tariff_data.backgroundPriceSection} isSelected = {tariff_data.isSelected} />
           ) 
         }
       </div>
