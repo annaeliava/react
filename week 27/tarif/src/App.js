3ccf429d3f33e4ece36adcc30899e355c1a404d0
annaeliava <annaeliavad@gmail.com>
2022-12-03 04:13:54 +0300

    fixed pressed

--- week 27/tarif/src/App.js
+++ week 27/tarif/src/App.js
@@ -1,20 +1,33 @@
+import React, {useState} from "react";
 import Tariff from "./components/Tariff";
 import styles from "./assets/styles/App.module.css";
 
 const tariff_data = [
-  { id: '1', price: '300', mbitQuantity: '10', backgroundTitle: '4A89DC', backgroundPriceSection: '5D9CEC' },
-  { id: '2', price: '450', mbitQuantity: '50', backgroundTitle: '37BC9B', backgroundPriceSection: '48CFAD' },
-  { id: '3', price: '550', mbitQuantity: '100', backgroundTitle: 'DA4453', backgroundPriceSection: 'ED5565', isSelected: true },
-  { id: '4', price: '1000', mbitQuantity: '200', backgroundTitle: '434A54', backgroundPriceSection: '656D78' },
+  { id: 1, price: '300', mbitQuantity: '10', backgroundTitle: '4A89DC', backgroundPriceSection: '5D9CEC', selected: false, pressed: false },
+  { id: 2, price: '450', mbitQuantity: '50', backgroundTitle: '37BC9B', backgroundPriceSection: '48CFAD', selected: false, pressed: false },
+  { id: 3, price: '550', mbitQuantity: '100', backgroundTitle: 'DA4453', backgroundPriceSection: 'ED5565', isSelected: true, selected: false, pressed: false },
+  { id: 4, price: '1000', mbitQuantity: '200', backgroundTitle: '434A54', backgroundPriceSection: '656D78', selected: false, pressed: false },
 ];
 
 function App() {
+  const [pressed, setPressed] = useState(tariff_data);
+
+  const handleChange = (cardID) => {
+    const pressedTariff = pressed.map(card => {
+      if(card.id === cardID) {
+        return {...card, pressed: !card.pressed}
+      }
+      return {...card, pressed: false}
+    }) 
+      setPressed(pressedTariff)
+  };
+
   return (
     <>
       <div className={styles.container}>
       {
-                tariff_data.map(card =>
-                    <Tariff data={card} />
+                pressed.map(card =>
+                    <Tariff card={card} key={card.id} handleChange={handleChange} />
                 )
             }
       </div>
--- week 27/tarif/src/components/Tariff.jsx
+++ week 27/tarif/src/components/Tariff.jsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './TariffCard.module.scss';
 
-Tariff.defaultProps = { // через static не вышло, выводил в problems 'declaration or statement expecred.'
+Tariff.defaultProps = {
     price: 'Sorry',
     mbitQuantity: '0',
     backgroundColor: '000000'
 };
 
 export default function Tariff(props) {
-    const { data } = props;
+    const { card, handleChange } = props;
 
-    const [pressed, setPressed] = useState(false);
-    const handleChange = () => {
-        setPressed(!pressed)
-    }
     return (
         <>
-            <div className={`${styles.card} ${data.isSelected ? styles.selected : ""} ${pressed ? styles.chosen : ''}`} onClick={handleChange}>
-                <div className={styles.title} style={{ backgroundColor: '#' + data.backgroundTitle }}>Безлимитный {data.price} </div>
-                <div className={styles.priceSection} style={{ backgroundColor: '#' + data.backgroundPriceSection }}>
+            <div className={`${styles.card} ${card.isSelected ? styles.selected : ""} ${card.pressed ? styles.chosen : ''}`} onClick={() => handleChange(card.id)}>
+                <div className={styles.title} style={{ backgroundColor: '#' + card.backgroundTitle }}>Безлимитный {card.price} </div>
+                <div className={styles.priceSection} style={{ backgroundColor: '#' + card.backgroundPriceSection }}>
                     <span id={styles.currency}>руб</span>
-                    <span id={styles.price}>{data.price}</span>
+                    <span id={styles.price}>{card.price}</span>
                     <span id={styles.forMonth}>/мес</span>
                 </div>
-                <div className={styles.mbitSection}>до {data.mbitQuantity} Мбит/сек</div>
+                <div className={styles.mbitSection}>до {card.mbitQuantity} Мбит/сек</div>
                 <div className={styles.volumeInclusion}>
                     <div>Объем включенного</div>
                     <div>трафика не ограничен</div>
                 </div>
-                <div className={styles.alert}>Вы выбрали тариф <span className={styles.chosenTariff}>"Безлимитный {data.price}"</span>!</div>
+                <div className={styles.alert}>Вы выбрали тариф <span className={styles.chosenTariff}>"Безлимитный {card.price}"</span>!</div>
             </div>
         </>
     );
